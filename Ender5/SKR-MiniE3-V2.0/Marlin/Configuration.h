9e6c63e0072a793809fa678c88ba59e317a669b9
Ole Voronin <ovoronin@sonatype.com>
2020-07-03 11:51:32 -0400

    Updated platformio.ini to latest marlin version

--- Ender5/SKR-MiniE3-V2.0/Marlin/Configuration.h
+++ Ender5/SKR-MiniE3-V2.0/Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(ovoronin, Ender-5-SKR-mE3-V2.0)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(ovoronin, ovo-Ender-5-SKR-mE3-V2.0)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
